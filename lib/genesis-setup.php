0e6211649cf5f081824a1252cb83f440a6091e44
Bryan Willis <willis.bryan@live.com>
2015-11-15 19:42:44 -0600

    Parent and Child Support Added

--- lib/genesis-setup.php
+++ lib/genesis-setup.php
@@ -1,87 +1,29 @@
 <?php
 
-// Add HTML5 markup structure
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+// Parent Theme Fix
+remove_theme_support( 'genesis-accessibility' );
+add_action( 'wp_enqueue_scripts', 'sp_disable_superfish' );
+function sp_disable_superfish() {
+	wp_deregister_script( 'superfish' );
+	wp_deregister_script( 'superfish-args' );
+}
 
-// Add viewport meta tag for mobile browsers
+
+// Child Theme Support
+add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
 add_theme_support( 'genesis-responsive-viewport' );
+add_theme_support( 'genesis-structural-wraps', array( 'menu-primary', 'menu-secondary', 'footer', 'jumbotron-inner', 'site-inner', ) );
 
-// Add featured image sizes
-add_image_size( 'bsg-featured-image', 1170, 630, true );
 
-// Add .container wrap classes
-add_filter( 'genesis_attr_structural-wrap', 'bsg_attributes_structural_wrap' );
-function bsg_attributes_structural_wrap( $attributes ) {
-    $attributes['class'] = 'container';
-    return $attributes;
-}
-
-add_theme_support( 'genesis-structural-wraps', array(
-        'nav',
-        'subnav',
-        'jumbotron-inner',
-        'site-inner',
-        'footer'
-) );
-
-// Remove item(s) from genesis admin screens
-add_action( 'genesis_admin_before_metaboxes', 'bsg_remove_genesis_theme_metaboxes' );
-
-// Remove item(s) from genesis customizer
+// Remove dynamic logo/text from genesis customizer
 add_action( 'customize_register', 'bsg_remove_genesis_customizer_controls', 20 );
-
-
-
-/**
- * Remove selected Genesis metaboxes from the Theme Settings and SEO Settings pages.
- *
- * @param string $hook The unique pagehook for the Genesis settings page
- */
-
-function bsg_remove_genesis_theme_metaboxes( $hook ) {
-    /** Theme Settings metaboxes */
-    //remove_meta_box( 'genesis-theme-settings-version',  $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-feeds',    $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-layout',   $hook, 'main' );
-    remove_meta_box( 'genesis-theme-settings-header',   $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-nav',      $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-breadcrumb', $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-comments', $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-posts',    $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-blogpage', $hook, 'main' );
-    //remove_meta_box( 'genesis-theme-settings-scripts',  $hook, 'main' );
-
-    /** SEO Settings metaboxes */
-    //remove_meta_box( 'genesis-seo-settings-doctitle',   $hook, 'main' );
-    //remove_meta_box( 'genesis-seo-settings-homepage',   $hook, 'main' );
-    //remove_meta_box( 'genesis-seo-settings-dochead',    $hook, 'main' );
-    //remove_meta_box( 'genesis-seo-settings-robots',     $hook, 'main' );
-    //remove_meta_box( 'genesis-seo-settings-archives',   $hook, 'main' );
-}
-
-
-
-/**
- * Filter to remove selected Genesis Customizer Menu controls
- *
- * @param instance of WP_Customize_Manager $wp_customize
- */
 function bsg_remove_genesis_customizer_controls( $wp_customize ) {
-    // remove Site Title/Logo: Dynamic Text or Image Logo option from Customizer
     $wp_customize->remove_control( 'blog_title' );
     return $wp_customize;
 }
 
 
-
-/* Remove Genesis Page Templates
- *
- * @author Bill Erickson
- * @link http://www.billerickson.net/remove-genesis-page-templates
- *
- * @param array $page_templates
- * @return array
- */
+// Remove page templates
 function bsg_be_remove_genesis_page_templates( $page_templates ) {
 	unset( $page_templates['page_archive.php'] );
 	unset( $page_templates['page_blog.php'] );
@@ -90,12 +32,20 @@ function bsg_be_remove_genesis_page_templates( $page_templates ) {
 add_filter( 'theme_page_templates', 'bsg_be_remove_genesis_page_templates' );
 
 
+// Add featured image sizes
+add_image_size( 'bsg-featured-image', 1170, 630, true );
 
 
-//* Do NOT include the opening php tag shown above. Copy the code shown below.
-//* Disable the superfish script
-add_action( 'wp_enqueue_scripts', 'sp_disable_superfish' );
-function sp_disable_superfish() {
-	wp_deregister_script( 'superfish' );
-	wp_deregister_script( 'superfish-args' );
+// Add container->wrap classes
+add_filter( 'genesis_attr_structural-wrap', 'bsg_attributes_structural_wrap' );
+function bsg_attributes_structural_wrap( $attributes ) {
+    $attributes['class'] = 'container';
+    return $attributes;
+}
+
+
+// Remove header settings from genesis admin screens
+add_action( 'genesis_admin_before_metaboxes', 'bsg_remove_genesis_theme_metaboxes' );
+function bsg_remove_genesis_theme_metaboxes( $hook ) {
+    remove_meta_box( 'genesis-theme-settings-header',   $hook, 'main' );
 }
