eca2041288176ff8caf3e849ba182e31441e90c8
Bryan Willis <willis.bryan@live.com>
2015-11-16 02:55:21 -0600

    Update footer-creds.php

--- lib/footer-creds.php
+++ lib/footer-creds.php
@@ -1,8 +1,20 @@
 <?php
 // Add your own footer and center it like bootstrap
 
+
+//add_action( 'genesis_footer', 'gb3_footer_widget_area_callback' );
+function gb3_footer_widget_area_callback() {
+    if ( is_active_sidebar( 'footer-inside' ) ) {
+                dynamic_sidebar( 'footer-inside' );
+    }
+}
+
+
 add_filter( 'genesis_footer_output', 'bsg_footer_creds_filter', 10, 3 );
 function bsg_footer_creds_filter( $creds ) {
 	$creds = '<p class="text-center">[footer_copyright] &middot; <a href="'. esc_url( home_url( '/' ) ) .'">'. get_bloginfo( 'name' ) .'</a></p>';
-	return $creds;
+        if ( is_active_sidebar( 'footer-inside' ) ) {
+              dynamic_sidebar( 'footer-inside' );
+        }
+        return $creds;
 }
