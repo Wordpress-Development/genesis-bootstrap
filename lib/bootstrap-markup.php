2a592bbdcaffadd2341584e8b6ac95dce471cf2c
Bryan Willis <willis.bryan@live.com>
2015-11-15 23:59:37 -0600

    Update bootstrap-markup.php

--- lib/bootstrap-markup.php
+++ lib/bootstrap-markup.php
@@ -84,9 +84,9 @@ function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
     }
 
     // sidebar-content          // supported
-    if ( 'sidebar-content' === $layout ) {
-        $classes_to_add['content'] = 'col-sm-9 col-sm-push-3';
-        $classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-pull-9';
+     if ( 'sidebar-content' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-12 col-md-8 col-lg-9 col-md-push-4 col-lg-push-3';
+        $classes_to_add['sidebar-primary'] = 'hidden-xs hidden-sm col-md-4 col-lg-3 col-md-pull-8 col-lg-pull-9';
     }
 
     // content-sidebar-sidebar  // supported
