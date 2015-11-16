061cc47a2d653c91d85e793f897f82bbaf1f9ae4
Bryan Willis <willis.bryan@live.com>
2015-11-16 02:03:55 -0600

    Update jumbotron.php

--- lib/jumbotron.php
+++ lib/jumbotron.php
@@ -1,24 +1,33 @@
 <?php
 
-// hide title and description on all pages other than front page
-add_action( 'template_redirect', 'bsg_title_area_hide_on_pages_other_than_front' );
+remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+add_action( 'genesis_header', 'jumbotron_widget_area_callback' );
 
-function bsg_title_area_hide_on_pages_other_than_front() {
-    //if ( !is_front_page() ) {
-        remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
-        remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
-    //}
+function jumbotron_widget_area_callback() {
+    if ( is_active_sidebar( 'jumbotron-widget-area' ) ) {
+        echo '<div class="jumbotron">';
+            genesis_structural_wrap( 'jumbotron-inner' );
+                dynamic_sidebar( 'jumbotron-widget-area' );
+            genesis_structural_wrap( 'jumbotron-inner', 'close' );
+        echo '</div>';
+   }
 }
 
-
-//add_action( 'template_redirect', 'bsg_title_area_jumbotron_unit_on_front_page' );
+/*
+    add_action( 'template_redirect', 'bsg_title_area_jumbotron_unit_on_front_page' );
 
 function bsg_title_area_jumbotron_unit_on_front_page() {
     if ( is_front_page() ) {
         add_action( 'genesis_site_title', 'bsg_jumbotron_unit_open', 2 );
         add_action( 'genesis_site_description', 'bsg_jumbotron_unit_close', 30 );
     }
+    else {
+        remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+        remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+    }
 }
+
 
 
 function bsg_jumbotron_unit_open() {
@@ -30,3 +39,5 @@ function bsg_jumbotron_unit_close() {
     genesis_structural_wrap( 'jumbotron-inner', 'close' );
     echo '</div>';
 }
+
+// */
