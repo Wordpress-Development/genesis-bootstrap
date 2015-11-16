825bd02840b9781681a24d6ec055d075432d8b6d
Bryan Willis <willis.bryan@live.com>
2015-11-16 02:05:51 -0600

    Update load-assets.php

--- lib/load-assets.php
+++ lib/load-assets.php
@@ -25,12 +25,15 @@ function bsg_load_bootstrap_stylesheet() {
         $stylesheet = plugins_url('bootstrap/css/bootstrap.min.css', __DIR__); // plugin_dir_url( __FILE__ )
     $stylesheet = apply_filters( 'bsg_css_url', $stylesheet );
     
-    if ( $stylesheet )
+    if ( $stylesheet ) {
         wp_enqueue_style( 'bsg-theme-css', $stylesheet, array(), CHILD_THEME_VERSION );
-        wp_register_script( 'bsg-bootstrap-js', plugins_url('bootstrap/js/javascript.min.js', __DIR__), array('jquery'), '3.3.5', true );
-
+        wp_enqueue_script( 'bsg-bootstrap-js', plugins_url('bootstrap/js/javascript.min.js', __DIR__), array('jquery'), '3.3.5', true );
+    
     //$css_file = file_get_contents($custom_css);
     //echo '<style type="text/css">' . $css_file . '</style>';
     //echo '<link rel="stylesheet" type="text/css" href="' . esc_url( $custom_css ) . '">' . "\n";
+
+    }
+    
 
 }
