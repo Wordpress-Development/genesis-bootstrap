55b9b2158444e6d6ea1b69dcdcbd885ed043f32f
Bryan Willis <willis.bryan@live.com>
2015-12-04 02:37:31 -0600

    Update mce-css.php

--- lib/mce-css.php
+++ lib/mce-css.php
@@ -3,7 +3,7 @@
 function plugin_mce_css( $mce_css ) {
 	if ( ! empty( $mce_css ) )
 		$mce_css .= ',';
-	$mce_css .= plugins_url( 'bootstrap/css/bootstrap.min.css', __FILE__ );
+	$mce_css .= plugins_url('bootstrap/css/bootstrap.min.css', __DIR__); 
 	return $mce_css;
 }
 add_filter( 'mce_css', 'plugin_mce_css' );
