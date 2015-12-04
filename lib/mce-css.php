0dc41dd9af013215fdfa9f588553023f021a6f27
Bryan Willis <willis.bryan@live.com>
2015-12-04 02:35:47 -0600

    Create mce-css.php

---
+++ lib/mce-css.php
@@ -0,0 +1,9 @@
+<?php
+
+function plugin_mce_css( $mce_css ) {
+	if ( ! empty( $mce_css ) )
+		$mce_css .= ',';
+	$mce_css .= plugins_url( 'bootstrap/css/bootstrap.min.css', __FILE__ );
+	return $mce_css;
+}
+add_filter( 'mce_css', 'plugin_mce_css' );
