6616f75c48b3f8d2cfccfd5a9e2f28bd812ddc82
Bryan Willis <willis.bryan@live.com>
2015-12-09 01:16:27 -0600

    Update bootstrap-markup.php

--- lib/bootstrap-markup.php
+++ lib/bootstrap-markup.php
@@ -1,57 +1,30 @@
  <?php
 
-
-// add bootstrap classes
-add_filter( 'genesis_attr_nav-primary',         'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_nav-secondary',       'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_site-header',         'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_site-inner',          'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_content-sidebar-wrap','bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_content',             'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_sidebar-primary',     'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_sidebar-secondary',   'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_archive-pagination',  'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_entry-content',       'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_entry-pagination',    'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_site-footer',         'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_comment',             'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_nav-footer',          'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_comment-reply',          'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_comment-header',          'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_comment-time-link',          'bsg_add_markup_class', 10, 2 );
-add_filter( 'genesis_attr_comment-media',          'bsg_add_markup_class', 10, 2 );
-
-
-
-// add bootstrap classes
-	$filters = array(
-                  'nav-primary',
-                  'nav-secondary',
-                  'site-header',
-                  'site-inner',
-                  'content-sidebar-wrap',
-                  'content',
-                  'sidebar-primary',
-                  'sidebar-secondary',
-                  'archive-pagination',
-                  'entry-content',
-                  'entry-pagination',
-                  'site-footer',
-                  'nav-footer',
-                  'comment-reply',
-                  'comment-header',
-                  'comment-time-link',
-                  'comment-media',
-                  'comment'	  
-    	);
-foreach ( $filters as $filter ) {
- add_filter(genesis_attr_$filter,'bsg_add_markup_class', 10, 2 );
+$atts = array(
+                'genesis_attr_nav-primary',
+                'genesis_attr_nav-secondary',
+                'genesis_attr_site-header',
+                'genesis_attr_site-inner',
+                'genesis_attr_content-sidebar-wrap',
+                'genesis_attr_content',
+                'genesis_attr_sidebar-primary',
+                'genesis_attr_sidebar-secondary',
+                'genesis_attr_archive-pagination',
+                'genesis_attr_entry-content',
+                'genesis_attr_entry-pagination',
+                'genesis_attr_site-footer',
+                'genesis_attr_nav-footer',
+                'genesis_attr_comment-reply',
+                'genesis_attr_comment-header',
+                'genesis_attr_comment-time-link',
+                'genesis_attr_comment-media',
+                'genesis_attr_comment'	  
+    		);
+foreach ( $atts as $context ) {
+	add_filter( $context, 'bsg_add_markup_class', 10, 2 );
 }
-
 function bsg_add_markup_class( $attr, $context ) {
-    // default classes to add
     $classes_to_add = apply_filters ('bsg-classes-to-add',
-        // default bootstrap markup values
         array(
             'nav-primary'               => 'navbar navbar-inverse navbar-static-top',
             'nav-secondary'             => 'navbar navbar-default navbar-static-top',
@@ -70,22 +43,18 @@ function bsg_add_markup_class( $attr, $context ) {
             'comment-time-link'         => 'text-muted small',
             'comment-media'             => 'media-left',
             'nav-footer'                => 'pull-right'
-
         ),
         $context,
         $attr
     );
-    // populate $classes_array based on $classes_to_add
     $value = isset( $classes_to_add[ $context ] ) ? $classes_to_add[ $context ] : array();
     if ( is_array( $value ) ) {
         $classes_array = $value;
     } else {
         $classes_array = explode( ' ', (string) $value );
     }
-    // apply any filters to modify the class
     $classes_array = apply_filters( 'bsg-add-class', $classes_array, $context, $attr );
     $classes_array = array_map( 'sanitize_html_class', $classes_array );
-    // append the class(es) string (e.g. 'span9 custom-class1 custom-class2')
     $attr['class'] .= ' ' . implode( ' ', $classes_array );
     return $attr;
 }
@@ -127,7 +96,6 @@ function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
     }
     return $classes_to_add;
 };
-
 function bsg_modify_classes_based_on_template( $classes_to_add, $context, $attr ) {
     $classes_to_add = bsg_layout_options_modify_classes_to_add( $classes_to_add );
     return $classes_to_add;
