33c851a7fc87bc67331eeded40d8338b9bc93042
Bryan Willis <willis.bryan@live.com>
2015-12-09 02:09:10 -0600

    Update nav.php

--- lib/nav.php
+++ lib/nav.php
@@ -6,21 +6,22 @@ if (
 ) {
     return;
 }
+
+
+remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
 // remove primary & secondary nav from default position
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+
 // add primary & secondary nav to top of the page
-add_action( 'genesis_before', 'genesis_do_nav' );
-add_action( 'genesis_before', 'genesis_do_subnav' );
+add_action( 'genesis_header', 'genesis_do_nav', 15 );
+add_action( 'genesis_header', 'genesis_do_subnav', 15 );
 
-// filter menu args for bootstrap walker and other settings
+
 add_filter( 'wp_nav_menu_args', 'bsg_nav_menu_args_filter' );
-
-// add bootstrap markup around the nav
-add_filter( 'wp_nav_menu', 'bsg_nav_menu_markup_filter', 10, 2 );
-
 function bsg_nav_menu_args_filter( $args ) {
-
     if (
         'primary' === $args['theme_location'] ||
         'secondary' === $args['theme_location']
@@ -30,23 +31,19 @@ function bsg_nav_menu_args_filter( $args ) {
         $args['fallback_cb'] = 'wp_bootstrap_navwalker::fallback';
         $args['walker'] = new wp_bootstrap_navwalker();
     }
-
     return $args;
 }
 
+add_filter( 'wp_nav_menu', 'bsg_nav_menu_markup_filter', 10, 2 );
 function bsg_nav_menu_markup_filter( $html, $args ) {
-    // only add additional Bootstrap markup to
-    // primary and secondary nav locations
     if (
         'primary'   !== $args->theme_location &&
         'secondary' !== $args->theme_location
     ) {
         return $html;
     }
-
     $data_target = "nav-collapse" . sanitize_html_class( '-' . $args->theme_location );
     $output = <<<EOT
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#{$data_target}">
             <span class="sr-only">Toggle navigation</span>
@@ -55,14 +52,11 @@ function bsg_nav_menu_markup_filter( $html, $args ) {
             <span class="icon-bar"></span>
           </button>
 EOT;
-        // only include blog name and description in the nav
-        // if it is the primary nav location
+
         if ( 'primary' === $args->theme_location ) {
             $output .= apply_filters( 'bsg_navbar_brand', bsg_navbar_brand_markup() );
         }
-
         $output .= '</div>'; // .navbar-header
-
         $output .= "<div class=\"collapse navbar-collapse\" id=\"{$data_target}\">";
             $output .= $html;
         $output .= '</div>'; // .collapse .navbar-collapse
@@ -77,6 +71,5 @@ function bsg_navbar_brand_markup() {
     '">';
         $output .= get_bloginfo( 'name' );
     $output .= '</a>';
-
     return $output;
 }
