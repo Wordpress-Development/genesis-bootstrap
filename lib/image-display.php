b9083022729e17508f825e1d58926f18c856d765
Bryan Willis <willis.bryan@live.com>
2015-12-10 00:06:57 -0600

    Update image-display.php

--- lib/image-display.php
+++ lib/image-display.php
@@ -3,8 +3,8 @@
 
 // Display Archive
 
-remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-add_action(    'genesis_entry_header',  'genesis_do_post_image', 0 );
+//remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+//add_action(    'genesis_entry_header',  'genesis_do_post_image', 0 );
 
 add_filter( "genesis_attr_entry-image", 'bsg_archive_image_attr_filter' );
 function bsg_archive_image_attr_filter( $attributes ) {
