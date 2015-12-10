2a841c75ef16baa8a0afc1fec95d93ccfffea561
Bryan Willis <willis.bryan@live.com>
2015-12-10 00:09:00 -0600

    Update sidebars.php

--- lib/sidebars.php
+++ lib/sidebars.php
@@ -5,7 +5,6 @@ unregister_sidebar( 'header-right' );
 
 add_action('widgets_init', 'register_additional_sidebars');
 function register_additional_sidebars() {
-    
     $jumbotron = array(
         'id'            => 'jumbotron-widget-area',
         'name'          => __( 'Jumbotron', 'gb3' ),
@@ -17,7 +16,6 @@ function register_additional_sidebars() {
         'after_title'   => '</h1>',
         
     );
-
     $footer = array(
         'id'            => 'footer-inside',
         'name'          => __( 'Footer', 'gb3' ),
@@ -29,8 +27,22 @@ function register_additional_sidebars() {
         'after_title'   => '</h3>',
         
     );
-    
     register_sidebar( $jumbotron );
     register_sidebar( $footer );
+}
 
+
+
+
+// Add widget area to single post
+add_theme_support( 'genesis-after-entry-widget-area' );
+remove_action( 'genesis_after_entry', 'genesis_after_entry_widget_area' );
+add_action( 'genesis_after_entry', 'amethyst_after_entry', 9 );
+function amethyst_after_entry() {
+   if ( ! is_singular( array( 'post' )) )
+        return;
+        genesis_widget_area( 'after-entry', array(
+            'before' => '<div class="after-entry widget-area">',
+            'after'  => '</div>',
+        ) );
 }
