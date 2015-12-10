d5d0af4a5aed50656173d2f208c1759083eb2c23
Bryan Willis <willis.bryan@live.com>
2015-12-09 19:29:57 -0600

    Update image-display.php

--- lib/image-display.php
+++ lib/image-display.php
@@ -8,7 +8,7 @@ add_action(    'genesis_entry_header',  'genesis_do_post_image', 0 );
 
 add_filter( "genesis_attr_entry-image", 'bsg_archive_image_attr_filter' );
 function bsg_archive_image_attr_filter( $attributes ) {
-    $attributes['class'] .= ' wp-post-image archive-featured-image';
+    $attributes['class'] .= ' wp-post-image archive-featured-image img-responsive';
     return $attributes;
 }
 
@@ -30,7 +30,7 @@ function bsg_single_featured_image() {
     }
 
     $featured_image_attr = apply_filters( 'bsg-featured-image-attr', array(
-        'class' => 'single-featured-image  img-responsive'
+        'class' => 'single-featured-image img-responsive'
     ) );
 
     $size = apply_filters( 'bsg-featured-image', 'bsg-featured-image' );
