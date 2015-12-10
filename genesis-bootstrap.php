5508b745758d4c69acaa8bd6102324e55f0bbd2f
Bryan Willis <willis.bryan@live.com>
2015-12-09 19:14:46 -0600

    Update genesis-bootstrap.php

--- genesis-bootstrap.php
+++ genesis-bootstrap.php
@@ -12,6 +12,7 @@ License URI:        http://opensource.org/licenses/MIT
 
 
 defined( 'WPINC' ) or die;
+
 register_activation_hook( __FILE__, 'activate_genesis_bootstrap_plugin_script_check' );
 function activate_genesis_bootstrap_plugin_script_check() {
 	if ( ! function_exists('genesis_pre') ) {
@@ -28,8 +29,6 @@ add_action('after_switch_theme', 'deactivate_genesis_bootstrap_plugin_script_che
 
 
 add_action( 'genesis_init', 'gs_constants', 15 );
-
-
 /**
  * This function defines the Genesis Child theme constants
  *
@@ -37,8 +36,6 @@ add_action( 'genesis_init', 'gs_constants', 15 );
  * CHILD_THEME_NAME, CHILD_THEME_URL, CHILD_DEVELOPER, CHILD_DEVELOPER_URL
  * Directories: CHILD_LIB_DIR, CHILD_IMAGES_DIR, CHILD_ADMIN_DIR, CHILD_JS_DIR, CHILD_CSS_DIR
  * URLs: CHILD_LIB, CHILD_IMAGES, CHILD_ADMIN, CHILD_JS, CHILD_CSS
- *
- * @since 1.1.0
  */
 function gs_constants() {
   $theme = wp_get_theme();
@@ -46,73 +43,55 @@ function gs_constants() {
     // Child theme (Change but do not remove)
         /** @type constant Child Theme Options/Settings. */
         define( 'CHILD_SETTINGS_FIELD', $theme->get('TextDomain') . '-settings' );
-        
         /** @type constant Text Domain. */
         define( 'CHILD_DOMAIN', $theme->get('TextDomain') );
-        
         /** @type constant Child Theme Version. */
         define( 'CHILD_THEME_VERSION', $theme->Version );
-        
         /** @type constant Child Theme Name, used in footer. */
         define( 'CHILD_THEME_NAME', $theme->Name );
-        
         /** @type constant Child Theme URL, used in footer. */
         define( 'CHILD_THEME_URL', $theme->get('ThemeURI') );
-        
     // Developer Information, see lib/admin/admin-functions.php
         /** @type constant Child Theme Developer, used in footer. */
         define( 'CHILD_DEVELOPER', $theme->Author );
-        
         /** @type constant Child Theme Developer URL, used in footer. */
         define( 'CHILD_DEVELOPER_URL', $theme->{'Author URI'}  );
-        
     // Define Directory Location Constants
         /** @type constant Child Theme Library/Includes URL Location. */
         define( 'CHILD_LIB_DIR',    CHILD_DIR . '/lib' );
-        
         /** @type constant Child Theme Images URL Location. */
         define( 'CHILD_IMAGES_DIR', CHILD_DIR . '/images' );
-        
         /** @type constant Child Theme Admin URL Location. */
         define( 'CHILD_ADMIN_DIR',  CHILD_LIB_DIR . '/admin' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_JS_DIR',     CHILD_DIR .'/js' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_CSS_DIR',    CHILD_DIR . '/css' );
-    
     // Define URL Location Constants
         /** @type constant Child Theme Library/Includes URL Location. */
         define( 'CHILD_LIB',        CHILD_URL . '/lib' );
-        
         /** @type constant Child Theme Images URL Location. */
         define( 'CHILD_IMAGES',     CHILD_URL . '/images' );
-        
         /** @type constant Child Theme Admin URL Location. */
         define( 'CHILD_ADMIN',      CHILD_LIB . '/admin' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_JS',     CHILD_URL .'/js' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_CSS',    CHILD_URL .'/css/' );    
 }
 
 
-
-function gb3_register_sidebar_defaults( $defaults ) {
-	$defaults['before_widget'] = '<section id="%1$s" class="panel panel-default widget %2$s"><div class="panel-body widget-wrap">';
-	return $defaults;
+// ADD BOOTSTRAP SIDEBAR WIDGET CLASSES
+if ( ! function_exists( 'gb3_register_sidebar_defaults' ) ) {
+	function gb3_register_sidebar_defaults( $defaults ) {
+		$defaults['before_widget'] = '<section id="%1$s" class="panel panel-default widget %2$s"><div class="panel-body widget-wrap">';
+		return $defaults;
+	}
 }
-
-function genesis_edit_widget_areas() {
-	
-    add_filter( 'genesis_register_widget_area_defaults', 'gb3_register_sidebar_defaults');
-    
-    
+function gb3_genesis_edit_widget_areas() {
+	add_filter( 'genesis_register_widget_area_defaults', 'gb3_register_sidebar_defaults');
 }
-add_action( 'genesis_setup', 'genesis_edit_widget_areas', 15 );
+add_action( 'genesis_setup', 'gb3_genesis_edit_widget_areas', 15 );
 
 
 
