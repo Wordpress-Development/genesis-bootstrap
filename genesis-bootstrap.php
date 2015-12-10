0b142e9b8cbdc297ed0724f898bde99092692ed4
Bryan Willis <willis.bryan@live.com>
2015-12-09 18:01:33 -0600

    Update genesis-bootstrap.php

--- genesis-bootstrap.php
+++ genesis-bootstrap.php
@@ -42,59 +42,42 @@ add_action( 'genesis_init', 'gs_constants', 15 );
  */
 function gs_constants() {
   $theme = wp_get_theme();
-    
     // Child theme (Change but do not remove)
         /** @type constant Child Theme Options/Settings. */
         define( 'CHILD_SETTINGS_FIELD', $theme->get('TextDomain') . '-settings' );
-        
         /** @type constant Text Domain. */
         define( 'CHILD_DOMAIN', $theme->get('TextDomain') );
-        
         /** @type constant Child Theme Version. */
         define( 'CHILD_THEME_VERSION', $theme->Version );
-        
         /** @type constant Child Theme Name, used in footer. */
         define( 'CHILD_THEME_NAME', $theme->Name );
-        
         /** @type constant Child Theme URL, used in footer. */
         define( 'CHILD_THEME_URL', $theme->get('ThemeURI') );
-        
-    // Developer Information, see lib/admin/admin-functions.php
+        // Developer Information, see lib/admin/admin-functions.php
         /** @type constant Child Theme Developer, used in footer. */
         define( 'CHILD_DEVELOPER', $theme->Author );
-        
         /** @type constant Child Theme Developer URL, used in footer. */
         define( 'CHILD_DEVELOPER_URL', $theme->{'Author URI'}  );
-        
-    // Define Directory Location Constants
+        // Define Directory Location Constants
         /** @type constant Child Theme Library/Includes URL Location. */
         define( 'CHILD_LIB_DIR',    CHILD_DIR . '/lib' );
-        
         /** @type constant Child Theme Images URL Location. */
         define( 'CHILD_IMAGES_DIR', CHILD_DIR . '/images' );
-        
         /** @type constant Child Theme Admin URL Location. */
         define( 'CHILD_ADMIN_DIR',  CHILD_LIB_DIR . '/admin' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_JS_DIR',     CHILD_DIR .'/js' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_CSS_DIR',    CHILD_DIR . '/css' );
-    
-    // Define URL Location Constants
+        // Define URL Location Constants
         /** @type constant Child Theme Library/Includes URL Location. */
         define( 'CHILD_LIB',        CHILD_URL . '/lib' );
-        
         /** @type constant Child Theme Images URL Location. */
         define( 'CHILD_IMAGES',     CHILD_URL . '/images' );
-        
         /** @type constant Child Theme Admin URL Location. */
         define( 'CHILD_ADMIN',      CHILD_LIB . '/admin' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_JS',     CHILD_URL .'/js' );
-        
         /** @type constant Child Theme JS URL Location. */
         define( 'CHILD_CSS',    CHILD_URL .'/css/' );    
 }
@@ -112,7 +95,7 @@ add_action( 'genesis_setup', 'genesis_edit_widget_areas', 15 );
 
 
 
-
+if ( ! function_exists( 'gb3_custom_theme_support' ) )
 function gb3_custom_theme_support() {
 $bsg_add_theme_support = array(
                         'bsg-sidebars',
@@ -133,9 +116,10 @@ $bsg_add_theme_support = array(
                         'bsg-footer'
     );
     
-    foreach ( $bsg_add_theme_support as $bsg_support ) {
-	           add_theme_support( $bsg_support );
-    } 
+        foreach ( $bsg_add_theme_support as $bsg_support ) {
+	    add_theme_support( $bsg_support );
+        }
+    }
 }
 add_action('after_setup_theme', 'gb3_custom_theme_support');
 
