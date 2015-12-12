6d389d499798109e818d9ce6920fc81dbed84e43
Bryan Willis <willis.bryan@live.com>
2015-12-12 17:00:49 -0600

    Add theme stylesheet

--- lib/bootstrap-genesis-css.php
+++ lib/bootstrap-genesis-css.php
@@ -1,1 +1,7 @@
-bootstrap-genesis.css
+<?php
+
+add_action( 'wp_enqueue_scripts', 'bsg_theme_default_style_fixes' ); 
+function bsg_theme_default_style_fixes() {
+    $stylesheet = plugins_url('bootstrap/css/bootstrap-genesis.css', __DIR__); // plugin_dir_url( __FILE__ )
+    wp_enqueue_style( 'bsg-theme-css', $stylesheet, array(), CHILD_THEME_VERSION );
+}
