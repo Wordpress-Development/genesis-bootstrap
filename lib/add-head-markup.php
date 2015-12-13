65aa5c2341231b02c4d2744fefd3418e7fd5ec08
Bryan Willis <willis.bryan@live.com>
2015-12-13 01:08:58 -0600

    Update add-head-markup.php

--- lib/add-head-markup.php
+++ lib/add-head-markup.php
@@ -11,5 +11,4 @@ function bsg_conditional_comments() {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php
-    // <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
 }
