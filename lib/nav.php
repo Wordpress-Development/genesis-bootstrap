ee4209bd4f1288574d6a742806ae03bbb728f079
Bryan Willis <willis.bryan@live.com>
2015-12-13 01:46:10 -0600

    Update nav.php

--- lib/nav.php
+++ lib/nav.php
@@ -63,15 +63,47 @@ EOT;
     return $output;
 }
 
+
+  // add customizer controls
+add_action( 'customize_register', 'bsg_navbar_brand_logo_customize_register' );
+function bsg_navbar_brand_logo_customize_register( $wp_customize ) {
+	$wp_customize->add_setting( 'brand_logo',
+             array(
+                'default' => '',
+                'type' => 'theme_mod',
+                'capability' => 'edit_theme_options',
+                'transport' => 'refresh',
+             )
+        );
+        $wp_customize->add_control( new WP_Customize_Image_Control(
+             $wp_customize,
+             'bsg_brand_logo',
+             array(
+                'label' => __( 'Navbar Logo', 'bsg' ),
+                'section' => 'title_tagline',
+                'settings' => 'brand_logo',
+                'priority' => 10,
+             )
+        ) );
+}
+
 function bsg_navbar_brand_markup() {
-    $output = '<a class="navbar-brand" id="logo" title="' .
-        esc_attr( get_bloginfo( 'description' ) ) .
-        '" href="' .
+        $brand_logo = get_theme_mod( 'brand_logo' );
+        if ( ! $brand_logo ) {
+            return $output;
+        }
+        $output = '<a class="navbar-brand" id="logo" title="' .
+            esc_attr( get_bloginfo( 'description' ) ) .
+            '" href="' .
             esc_url( home_url( '/' ) ) .
-    '">';
-        $output .= get_bloginfo( 'name' );
-    $output .= '</a>';
-    return $output;
+            '">';
+        $output .= '<img src="' .
+            $brand_logo .
+            '" alt="' .
+            esc_attr( get_bloginfo( 'name' ) ) .
+            '">';
+        $output .= '</a>';
+        return $output;
 }
 
 
