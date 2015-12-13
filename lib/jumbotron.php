766404dd68d7b3196c67273893d91ccbf46ce9fd
Bryan Willis <willis.bryan@live.com>
2015-12-13 01:30:22 -0600

    Update jumbotron.php

--- lib/jumbotron.php
+++ lib/jumbotron.php
@@ -2,12 +2,31 @@
 
 remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
 remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+
+add_action('widgets_init', 'register_jumbotron_sidebar');
+function register_jumbotron_sidebar() {
+    
+    $jumbotron = array(
+        'id'            => 'jumbotron-widget-area',
+        'name'          => __( 'Jumbotron', 'gb3' ),
+        'description'   => __( 'This is located inside the jumbotron header.',  'gb3'  ),
+        'class'         => '',
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h1>',
+        'after_title'   => '</h1>',
+        
+    );  
+    register_sidebar( $jumbotron );
+}
+
+
 add_action( 'genesis_after_header', 'do_jumbotron_widget_area' );
-
-
 function do_jumbotron_widget_area() {
 	do_action('jumbotron');
 }
+
 
 
 add_action( 'jumbotron', 'jumbotron_widget_area_callback' );
@@ -19,43 +38,9 @@ if ( is_active_sidebar( 'jumbotron-widget-area' ) ): ?>
         	<div class="jumbotron">
                 	<?php genesis_structural_wrap( 'jumbotron-inner' ); ?>
                     		<?php dynamic_sidebar( 'jumbotron-widget-area' ); ?>
-                		<?php genesis_structural_wrap( 'jumbotron-inner', 'close' ); ?>
-            		</div>
-            	<?php genesis_structural_wrap( 'jumbotron-outer', close ); ?>
-        </header>
+                	<?php genesis_structural_wrap( 'jumbotron-inner', 'close' ); ?>
+            	</div>
+        <?php genesis_structural_wrap( 'jumbotron-outer', close ); ?>
+</header>
 <?php endif;
-
 }
-
-
-
-
-
-
-/*
-    add_action( 'template_redirect', 'bsg_title_area_jumbotron_unit_on_front_page' );
-
-function bsg_title_area_jumbotron_unit_on_front_page() {
-    if ( is_front_page() ) {
-        add_action( 'genesis_site_title', 'bsg_jumbotron_unit_open', 2 );
-        add_action( 'genesis_site_description', 'bsg_jumbotron_unit_close', 30 );
-    }
-    else {
-        remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
-        remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
-    }
-}
-
-
-
-function bsg_jumbotron_unit_open() {
-echo '<div class="jumbotron">';
-genesis_structural_wrap( 'jumbotron-inner' );
-}
-
-function bsg_jumbotron_unit_close() {
-    genesis_structural_wrap( 'jumbotron-inner', 'close' );
-    echo '</div>';
-}
-
-// */
