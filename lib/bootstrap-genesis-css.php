c2dc4e6debaecce81312f014ce65dcd7f4beb7fe
Bryan Willis <willis.bryan@live.com>
2015-12-13 02:30:56 -0600

    Update bootstrap-genesis-css.php

--- lib/bootstrap-genesis-css.php
+++ lib/bootstrap-genesis-css.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'bsg_theme_default_style_fixes' ); 
+add_action( 'wp_enqueue_scripts', 'bsg_theme_default_style_fixes', 999 ); 
 function bsg_theme_default_style_fixes() {
     $stylesheet = plugins_url('bootstrap/bootstrap-genesis.css', __DIR__); // plugin_dir_url( __FILE__ )
     wp_enqueue_style( 'bsg-theme-css', $stylesheet, array(), CHILD_THEME_VERSION );
