ab40a7244358d5b653e973dd58fda4e6c14a8bd4
Bryan Willis <willis.bryan@live.com>
2015-12-13 01:03:34 -0600

    Update responsive-videos-widget.php

--- lib/responsive-videos-widget.php
+++ lib/responsive-videos-widget.php
@@ -1,12 +1,12 @@
 <?php
 
-class Adapter_Responsive_Video extends WP_Widget {
+class Bootstrap_Responsive_Video extends WP_Widget {
 
 	function __construct() {
-		 $options = array( 'classname' => 'adapter-responsive-video' ,
-				'description' => __( 'Video from YouTube, Vimeo, and more.' , 'adapter-responsive-video' ) ,
+		 $options = array( 'classname' => 'bootstrap-responsive-video' ,
+				'description' => __( 'Video from YouTube, Vimeo, and more.' , 'bootstrap-responsive-video' ) ,
 		 );
-		 $this->WP_Widget( 'adapter_responsive_video' , __( 'GB3 Responsive Video Embed' , 'adapter-responsive-video' ) , $options );
+		 $this->WP_Widget( 'bootstrap_responsive_video' , __( 'Bootstrap Responsive Video Embed' , 'bootstrap-responsive-video' ) , $options );
 	}
 
 	function form( $instance ) {
@@ -14,7 +14,7 @@ class Adapter_Responsive_Video extends WP_Widget {
 		?>
 			<p>
 				<label for="<?php echo $this->get_field_id( 'video_url' ); ?>">
-	 			       <?php _e( 'Video url' , 'adapter-responsive-video' ); ?>
+	 			       <?php _e( 'Video url' , 'bootstrap-responsive-video' ); ?>
 				</label>
 				<input type="text" value="<?php echo esc_url( $video_url ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'video_url' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'video_url' ) ); ?>" placeholder="e.g. www.youtube.com/watch?v=mOXRZ0eYSA0" \>
 			</p>
@@ -25,9 +25,9 @@ class Adapter_Responsive_Video extends WP_Widget {
 		$instance = $previous_instance;
 		$video_url = isset( $new_instance[ 'video_url' ] ) ? $new_instance[ 'video_url' ] : "";
 		if ( $video_url ) {
-			$raw_iframe_code = arv_get_raw_iframe_code( $video_url );
-			$instance[ 'video_source' ] = arv_get_iframe_attribute( $raw_iframe_code , 'src' );
-			$instance[ 'aspect_ratio_class' ] = arv_get_class_for_aspect_ratio( $raw_iframe_code );
+			$raw_iframe_code = gb3__get_raw_iframe_code( $video_url );
+			$instance[ 'video_source' ] = gb3_get_iframe_attribute( $raw_iframe_code , 'src' );
+			$instance[ 'aspect_ratio_class' ] = gb3_get_class_for_aspect_ratio( $raw_iframe_code );
 			$instance[ 'video_url' ] = strip_tags( $video_url );
 		}
 		return $instance;
@@ -44,13 +44,15 @@ class Adapter_Responsive_Video extends WP_Widget {
 
 }
 
-function arv_get_raw_iframe_code( $url ) {
+
+
+function gb3_get_raw_iframe_code( $url ) {
 	$raw_code = wp_oembed_get( esc_url( $url ) );
 	return $raw_code;
 }
 
 function get_bootstrap_responsive_video( $src , $class ) {
-	$max_width = apply_filters( 'arv_video_max_width' , '580' );
+	$max_width = apply_filters( 'gb3_video_max_width' , '580' );
 	return
 		"<div class='responsive-video-container' style='max-width:{$max_width}px'>
 			<div class='embed-responsive {$class}'>
@@ -60,7 +62,7 @@ function get_bootstrap_responsive_video( $src , $class ) {
 		 </div>\n";
 }
 
-function arv_get_iframe_attribute( $iframe , $attribute ) {
+function gb3_get_iframe_attribute( $iframe , $attribute ) {
 	$pattern	= '/<iframe.*?' . $attribute . '=\"([^\"]+?)\"/';
 	preg_match( $pattern , $iframe , $matches );
 	if ( isset( $matches[ 1 ] ) ) {
@@ -68,56 +70,56 @@ function arv_get_iframe_attribute( $iframe , $attribute ) {
 	}
 }
 
-function arv_get_class_for_aspect_ratio( $embed_code ) {
-	$bootstrap_apect_ratio = get_bootstrap_aspect_ratio( $embed_code );
+function gb3_get_class_for_aspect_ratio( $embed_code ) {
+	$bootstrap_apect_ratio = gb3_get_bootstrap_aspect_ratio( $embed_code );
 	return	'embed-responsive-' . $bootstrap_apect_ratio;
 }
 
-function get_bootstrap_aspect_ratio( $embed_code ) {
-	$aspect_ratio = arv_get_raw_aspect_ratio( $embed_code );
+function gb3_get_bootstrap_aspect_ratio( $embed_code ) {
+	$aspect_ratio = gb3_get_raw_aspect_ratio( $embed_code );
 	if ( is_ratio_closer_to_four_by_three( $aspect_ratio ) ) {
 		return '4by3';
 	}
 	return '16by9';
 }
 
-function arv_get_raw_aspect_ratio( $embed_code ) {
-	$embed_width = arv_get_iframe_attribute( $embed_code , 'width' );
-	$embed_height =	arv_get_iframe_attribute( $embed_code , 'height' );
+function gb3_get_raw_aspect_ratio( $embed_code ) {
+	$embed_width = gb3_get_iframe_attribute( $embed_code , 'width' );
+	$embed_height =	gb3_get_iframe_attribute( $embed_code , 'height' );
 	if ( $embed_width && $embed_height ) {
 		$aspect_ratio = floatval( $embed_width ) / floatval( $embed_height );
 		return $aspect_ratio;
 	}
 }
 
-function is_ratio_closer_to_four_by_three( $ratio ) {
-	$difference_from_four_by_three = get_difference_from_four_by_three( $ratio );
-	$difference_from_sixteen_by_nine = get_difference_from_sixteen_by_nine( $ratio );
+function gb3_is_ratio_closer_to_four_by_three( $ratio ) {
+	$difference_from_four_by_three = gb3_get_difference_from_four_by_three( $ratio );
+	$difference_from_sixteen_by_nine = gb3_get_difference_from_sixteen_by_nine( $ratio );
 	return ( $difference_from_four_by_three < $difference_from_sixteen_by_nine );
 }
 
-function get_difference_from_four_by_three( $value ) {
+function gb3_get_difference_from_four_by_three( $value ) {
 	$four_by_three = 1.3333;
 	$difference_from_four_by_three = abs( $value - $four_by_three );
 	return $difference_from_four_by_three;
 }
 
-function get_difference_from_sixteen_by_nine( $value ) {
+function gb3_get_difference_from_sixteen_by_nine( $value ) {
 	$sixteen_by_nine = 1.777;
 	$difference_from_sixteen_by_nine = abs( $value - $sixteen_by_nine );
 	return $difference_from_sixteen_by_nine;
 }
 
-add_filter( 'embed_oembed_html' , 'arv_result_filter' );
-function arv_result_filter( $raw_embed_code ) {
-	$src = arv_get_iframe_attribute( $raw_embed_code , 'src' );
-	$class = arv_get_class_for_aspect_ratio( $raw_embed_code );
-	$bootstrap_markup = get_bootstrap_responsive_video( $src , $class );
+add_filter( 'embed_oembed_html' , 'gb3_responsive_embed_result_filter' );
+function gb3_responsive_embed_result_filter( $raw_embed_code ) {
+	$src = gb3_get_iframe_attribute( $raw_embed_code , 'src' );
+	$class = gb3_get_class_for_aspect_ratio( $raw_embed_code );
+	$bootstrap_markup = gb3_get_bootstrap_responsive_video( $src , $class );
 	return "<div class='post-responsive-video'>" . $bootstrap_markup . "</div>";
 }
 
 
-add_action( 'widgets_init' , 'arv_register_widget' );
-function arv_register_widget() {
-	register_widget( 'Adapter_Responsive_Video' );
+add_action( 'widgets_init' , 'gb3_register_widget_responsive_embed' );
+function gb3_register_widget_responsive_embed() {
+	register_widget( 'Bootstrap_Responsive_Video' );
 }
