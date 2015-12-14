40029f316968ac464a84ba6e1e3b5852374fd789
Bryan Willis <willis.bryan@live.com>
2015-12-14 00:11:32 -0600

    Update add-head-markup.php

--- lib/add-head-markup.php
+++ lib/add-head-markup.php
@@ -1,14 +1,17 @@
 <?php
-remove_action(  'genesis_doctype', 'genesis_do_doctype' );
-add_action(     'genesis_doctype', 'bsg_conditional_comments' );
 
-function bsg_conditional_comments() {
-    ?>
-<!DOCTYPE html>
-<html lang="en" class="no-js" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php
+remove_theme_support( 'genesis-responsive-viewport' );
+remove_action(  'genesis_doctype', 'genesis_do_doctype' );
+
+add_action( 'genesis_doctype', 'twbsg_conditional_comments' );
+function twbsg_conditional_comments() {
+    $output = <<<EOT
+        <!DOCTYPE html>
+        <html lang="en" class="no-js" dir="ltr">
+        <head profile="http://gmpg.org/xfn/11">
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+EOT;
+    return $output;
 }
