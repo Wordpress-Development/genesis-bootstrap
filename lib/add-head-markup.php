c85bdc270d59385b07aafb5e46e80b2f77c2e5da
Bryan Willis <willis.bryan@live.com>
2015-12-14 05:15:45 -0600

    Update add-head-markup.php

--- lib/add-head-markup.php
+++ lib/add-head-markup.php
@@ -1,5 +1,9 @@
 <?php
 
+/*----------------------------------------------------------------------------*
+ * Bootstrap Head Support
+ *----------------------------------------------------------------------------*/
+ 
 remove_theme_support( 'genesis-responsive-viewport' );
 remove_action(  'genesis_doctype', 'genesis_do_doctype' );
 remove_action( 'wp_head', 'genesis_do_meta_pingback' );
@@ -28,5 +32,5 @@ function html5_shiv_respond_js_add_last() { ?>
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->	
+<![endif]-->
 <?php }
