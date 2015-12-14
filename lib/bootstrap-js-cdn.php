26ad7f285a29f9fdd0755dcb919c7a913f7b2fc1
Bryan Willis <willis.bryan@live.com>
2015-12-14 04:14:50 -0600

    Update bootstrap-js-cdn.php

--- lib/bootstrap-js-cdn.php
+++ lib/bootstrap-js-cdn.php
@@ -2,10 +2,10 @@
 
 /* Bootstrap CDN */
 
-function twbsg_bootstrap_js() {
+function twbsg_bootstrap_js_url() {
 $bs_cdn_url = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js';
 $bs_plugin_url = plugins_url('bootstrap/js/javascript.min.js', __DIR__);
-$cdnIsUp = get_transient( 'use_bs_cdn' );
+$cdnIsUp = get_transient( 'twbs_cdn_js' );
 if ( $cdnIsUp ) {
 	$load_source = $bs_cdn_url;
 } else {
@@ -14,16 +14,13 @@ if ( $cdnIsUp ) {
 		$load_source = $bs_plugin_url;
 	}
 	else {
-		$cdnIsUp = set_transient( 'use_bs_cdn', true, MINUTE_IN_SECONDS * 20 );
+		$cdnIsUp = set_transient( 'twbs_cdn_js', true, MINUTE_IN_SECONDS * 20 );
 		$load_source = $bs_cdn_url;
 	}
 }
 return $load_source;
 }
-
-
-add_action('wp_enqueue_scripts', 'brw_enqueue_bootstrap');
-function brw_enqueue_bootstrap() { 
-    wp_register_script( 'bootstrap', twbsg_bootstrap_js(), array('jquery'), 3.3.6, true); 
-    wp_enqueue_script('bootstrap');
-}
+add_action('wp_enqueue_scripts', function() {
+	wp_register_script('bootstrap', twbsg_bootstrap_js_url(), array('jquery'), '3.3.6', true); 
+    	wp_enqueue_script('bootstrap');
+});
