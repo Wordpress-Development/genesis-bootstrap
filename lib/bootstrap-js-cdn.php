cb72541811ca63683093b808b17b890906fc0396
Bryan Willis <willis.bryan@live.com>
2015-12-14 03:30:34 -0600

    Update bootstrap-js-cdn.php

--- lib/bootstrap-js-cdn.php
+++ lib/bootstrap-js-cdn.php
@@ -1,30 +1,29 @@
 <?php
 
-function brw_load_bootstrap() {
-  
-$get_the_url = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js';
-$get_the_local = 'http://gb3.wpengine.com/wp-content/plugins/genesis-bootstrap-master/bootstrap/js/javascript.min.js';
-  
+/* Bootstrap CDN */
+
+function twbsg_bootstrap_js() {
+$bs_cdn_url = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js';
+$bs_plugin_url = plugins_url('bootstrap/js/javascript.min.js', __DIR__);
 $cdnIsUp = get_transient( 'use_bs_cdn' );
 if ( $cdnIsUp ) {
-    $load_source = $get_the_url;
+	$load_source = $bs_cdn_url;
 } else {
-    $cdn_response = wp_remote_get( $get_the_url );
-    if( is_wp_error( $cdn_response ) || wp_remote_retrieve_response_code($cdn_response) != '200' ) {
-        $load_source = $get_the_local;
-    }
-    else {
-        $cdnIsUp = set_transient( 'use_bs_cdn', true, MINUTE_IN_SECONDS * 20 );
-        $load_source = $get_the_url;
-    }
- }
-  
-	return $load_source;
+	$cdn_response = wp_remote_get( $bs_cdn_url );
+	if( is_wp_error( $cdn_response ) || wp_remote_retrieve_response_code($cdn_response) != '200' ) {
+		$load_source = $bs_plugin_url;
+	}
+	else {
+		$cdnIsUp = set_transient( 'use_bs_cdn', true, MINUTE_IN_SECONDS * 20 );
+		$load_source = $bs_cdn_url;
+	}
+}
+return $load_source;
 }
 
 
 add_action('wp_enqueue_scripts', 'brw_enqueue_bootstrap');
 function brw_enqueue_bootstrap() { 
-    wp_register_script( 'bootstrap1', brw_load_bootstrap(), array('jquery'), 3.3, true); 
-    wp_enqueue_script('bootstrap1'); 
+    wp_register_script( 'bootstrap', twbsg_bootstrap_js(), array('jquery'), 3.3.6, true); 
+    wp_enqueue_script('bootstrap');
 }
