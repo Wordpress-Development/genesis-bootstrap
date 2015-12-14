64102e13452fe8626487af0a3c6a961aa63ed029
Bryan Willis <willis.bryan@live.com>
2015-12-14 00:33:05 -0600

    Update add-head-markup.php

--- lib/add-head-markup.php
+++ lib/add-head-markup.php
@@ -5,13 +5,19 @@ remove_action(  'genesis_doctype', 'genesis_do_doctype' );
 
 add_action( 'genesis_doctype', 'twbsg_conditional_comments' );
 function twbsg_conditional_comments() {
-    $output = <<<EOT
-        <!DOCTYPE html>
-        <html lang="en" class="no-js" dir="ltr">
-        <head profile="http://gmpg.org/xfn/11">
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+$doctype = <<<EOT
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head profile="http://gmpg.org/xfn/11">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
 EOT;
-    return $output;
+    
+    $doctype = preg_replace('/\t/', '', $doctype);
+    echo $doctype;
+    
 }
