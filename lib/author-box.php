3f90ba809f4b0e3cdb47f88b5ed49628cc39b46c
Bryan Willis <willis.bryan@live.com>
2015-12-18 17:50:05 -0600

    Update author-box.php

--- lib/author-box.php
+++ lib/author-box.php
@@ -26,6 +26,7 @@ function altitude_author_box_gravatar( $size ) {
 // */
 
 
+
 /*-----------------------------------------------------------------------------------*/
 /* Add custom meta fields                                                            */
 /*-----------------------------------------------------------------------------------*/
@@ -48,7 +49,79 @@ function rv_custom_profile_fields( $contactmethods ) {
 /*-----------------------------------------------------------------------------------*/
 /* Removes default Genesis Author Box - Add custom box with custom user meta fields  */
 /*-----------------------------------------------------------------------------------*/
-//* 
+//*
+function twbsgen_genesis_do_author_box_single() {
+if ( is_single() ) {
+	$gravatar = get_avatar( get_the_author_meta( 'ID' ), 250 ); // Author Gravatar
+	$display_name = get_the_author_meta( 'display_name' ); // Author Display Name
+	$authinfo = get_the_author_meta( 'description' ); // Author Bio 
+	$facebook = esc_url( htmlentities( get_the_author_meta( 'facebook_custom' ) ), array( 'https', 'http' ) );
+	$linkedin = esc_url( htmlentities( get_the_author_meta( 'linkedin_custom' ) ), array( 'https', 'http' ) );
+	$twitter = esc_url( htmlentities( get_the_author_meta( 'twitter_custom' ) ), array( 'https', 'http' ) ); 
+	$gplus = esc_url( htmlentities( get_the_author_meta( 'gplus_custom' ) ), array( 'https', 'http' ) );
+	$website = esc_url( htmlentities( get_the_author_meta( 'url' ) ), array( 'https', 'http' ) );
+?>
+<!-- Genesis Bootstrap Author Box -->
+<section class="author-box container">
+	<div class="panel panel-default">
+   		<div class="panel-body">
+			<div class="row">
+				<div class="col-xs-6 col-xs-offset-3 col-sm-3 col-sm-offset-0">			
+					<img class="img-responsive img-circle center-block" src="<?php echo $gravatar; ?>">	
+				</div>
+				<!-- col-sm-3 -->
+				<div class="col-xs-12 col-sm-9">
+					<hr class="visible-xs-block">	
+					<div class="author-box-intro small text-muted">
+						About the Author
+					</div>
+					<!-- .author-box-intro -->
+					<div class="author-inline-block">
+						<h4 class="author-box-title">
+							<span class="author-name text-primary"><?php echo $display_name; ?></span>
+							<small class="author-social-links small">
+							<?php
+							if ( $twitter || $facebook || $linkedin || $gplus || $website ) {
+								if ( $twitter )
+									echo '<a href="' . $twitter . '" class="author-social-link twitter" target="_blank"><i class="fa fa-2x fa-twitter"></i></a>';
+								if ( $facebook )
+									echo '<a href="' . $facebook . '" class="author-social-link facebook" target="_blank"><i class="fa fa-2x fa-facebook"></i></a>';
+								if ( $gplus )
+									echo '<a href="' . $gplus . '" class="author-social-link gplus" target="_blank"><i class="fa fa-2x fa-google-plus"></i></a>';
+								if ( $linkedin )
+									echo '<a href="' . $linkedin . '" class="author-social-link linkedin" target="_blank"><i class="fa fa-2x fa-linkedin"></i></a>';
+							}
+							?>	
+							</small>
+							<!-- author-social-links -->
+						</h4>
+					</div>
+					<div class="author-box-content">
+						<?php echo $authinfo; ?>
+					</div>
+					<!-- author-box-content -->
+				</div>
+				<!-- col -->
+			</div>
+			<!-- row -->
+		</div>
+		<!-- panel-body -->
+	</div>
+	<!-- panel -->
+
+</section>
+<!-- author-box -->
+<? }
+}
+remove_action( 'genesis_after_entry', 'genesis_do_author_box_single', 8 ); // Remove Genesis Author Box
+add_action( 'genesis_after_entry', 'twbsgen_genesis_do_author_box_single', 8 ); // Add bootstrap author box instead
+//*/
+
+
+
+
+
+/* 
 function twbsgen_genesis_do_author_box_single() { 
   if ( is_single() ) {
   ?>
