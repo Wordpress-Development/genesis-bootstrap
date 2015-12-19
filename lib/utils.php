1828a0b348405e3e14285931331f1aa36b861de2
Bryan Willis <willis.bryan@live.com>
2015-12-19 00:09:16 -0600

    Update utils.php

--- lib/utils.php
+++ lib/utils.php
@@ -12,3 +12,17 @@ function tbg3_wrap_container_fluid( $output, $original_output ) {
 }
 
 
+/*
+function jmw_filter_footer_widgets_structural_wrap( $output, $original_output ) {
+  	switch ( $original_output ) {
+		case 'open':
+			$output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
+			break;
+		case 'close':
+			$output = '</div>';
+			break;
+	}
+  	return $output;
+}
+add_filter( "genesis_structural_wrap-site-inner", 'jmw_filter_footer_widgets_structural_wrap', 999, 2);
+// */
