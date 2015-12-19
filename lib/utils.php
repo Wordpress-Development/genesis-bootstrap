1d01b556dd16a9ac2795ea95d8f130c4d4e83a96
Bryan Willis <willis.bryan@live.com>
2015-12-19 00:14:50 -0600

    Update utils.php

--- lib/utils.php
+++ lib/utils.php
@@ -1,9 +1,14 @@
 <?php 
-
 // Utility Helper Functions
 
-// Replace container with container fluid
-
+/**
+* Replace theme supported container wraps with container fluid wraps using genesis_structural_wrap.
+* Related File: https://github.com/Wordpress-Development/genesis-bootstrap/blob/master/lib/genesis-setup.php#L30
+* Docs: https://github.com/Wordpress-Development/genesis-bootstrap/wiki/Bootstrap-Fluid-Containers
+* 
+* @param string $output The markup to be returned
+* @param string $original_output Set to either 'open' or 'close'
+*/
 function tbg3_wrap_container_fluid( $output, $original_output ) {
   if( 'open' == $original_output ) {
 	   $output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
@@ -11,9 +16,8 @@ function tbg3_wrap_container_fluid( $output, $original_output ) {
   return $output;
 }
 
-
 /*
-function jmw_filter_footer_widgets_structural_wrap( $output, $original_output ) {
+function brw_filter_footer_widgets_structural_wrap( $output, $original_output ) {
   	switch ( $original_output ) {
 		case 'open':
 			$output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
@@ -24,5 +28,5 @@ function jmw_filter_footer_widgets_structural_wrap( $output, $original_output ) 
 	}
   	return $output;
 }
-add_filter( "genesis_structural_wrap-site-inner", 'jmw_filter_footer_widgets_structural_wrap', 999, 2);
+add_filter( 'genesis_structural_wrap-site-inner', 'brw_filter_footer_widgets_structural_wrap', 999, 2);
 // */
