0f7a4ea946d20d2f0e7ba6ec6f67884f3553e71d
Bryan Willis <willis.bryan@live.com>
2016-01-05 00:39:54 -0600

    Update layouts.php

--- lib/layouts.php
+++ lib/layouts.php
@@ -1,12 +1,4 @@
 <?php
-
-add_theme_support( 'genesis-structural-wraps', array( 
-	'menu-primary', 
-	'menu-secondary', 
-	'footer', 
-	'jumbotron-inner', 
-	'site-inner' 
-) );
 	
 add_filter( 'genesis_attr_structural-wrap', 'bsg_attributes_structural_wrap' );
 function bsg_attributes_structural_wrap( $attributes ) {
