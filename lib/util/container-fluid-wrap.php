d05f5489eea73ca079382e143048c6008e187ffe
Bryan Willis <willis.bryan@live.com>
2016-01-05 16:37:52 -0600

    Update container-fluid-wrap.php

--- lib/util/container-fluid-wrap.php
+++ lib/util/container-fluid-wrap.php
@@ -9,14 +9,50 @@
 * @param string $original_output Set to either 'open' or 'close'
 */
 function tbg3_wrap_container_fluid( $output, $original_output ) {
-  if( 'open' == $original_output ) {
-	   $output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
-  }
+	switch ( $original_output ) {
+		case 'open':
+	   		$output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
+  		break;
+	}
   return $output;
 }
 
+function __bsg_wrap_all_container_fluid() {
+    $genesis_atts = array(
+	'menu-primary', 
+	'menu-secondary', 
+	'footer', 
+	'jumbotron-inner', 
+	'site-inner' 
+    );
+    foreach ( $genesis_atts as $context ) {
+	$context = "genesis_attr_$context";
+	add_filter( $context, 'bsg-classes-to-add', 10, 2 );
+    }
+}
 
-/*
+/* Useage
+-----------
+
+On single page templates add this anywhere before genesis(); function :
+
+__bsg_wrap_all_container_fluid();
+
+Or use in functions.php :
+
+function mytheme_return_full_wrap_pages(){
+	if ( is_page('foo') ||  is_page('bar') {
+		__bsg_wrap_all_container_fluid();
+	}
+}
+add_action('genesis_before', 'mytheme_return_full_wrap_pages');
+
+*/
+
+
+
+/* # Other notes / experiments below - not tested in anyway
+
 function brw_filter_footer_widgets_structural_wrap( $output, $original_output ) {
   	switch ( $original_output ) {
 		case 'open':
@@ -29,4 +65,11 @@ function brw_filter_footer_widgets_structural_wrap( $output, $original_output ) 
   	return $output;
 }
 add_filter( 'genesis_structural_wrap-site-inner', 'brw_filter_footer_widgets_structural_wrap', 99, 2);
+// */
+
+/*
+function foo($wrap_context) {
+    add_filter( 'genesis_structural_wrap-'{$wrap_context}, 'brw_filter_footer_widgets_structural_wrap', 99, 2);
+}
+foo('site-inner');
 // */
