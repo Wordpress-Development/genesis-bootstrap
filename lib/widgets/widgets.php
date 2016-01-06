bdf330d4296e1ee6ba762d90eb7efbbb0a4abc49
Bryan Willis <willis.bryan@live.com>
2016-01-06 03:25:24 -0600

    Update widgets.php

--- lib/widgets/widgets.php
+++ lib/widgets/widgets.php
@@ -1,33 +1,4 @@
 <?php
-/******************************************************************************************/
-/*  Sidebar Filters - Remove Widget Panel in Header                                       */
-/******************************************************************************************/
-//*
-function wordpress_widgets_booststrapped_widget_params( $params ) { 
-  if ('header-right' === $params[0]['id']) {
-    $params[0]['before_widget'] = ''; // before sidebar widget 
-    $params[0]['after_widget']  = ''; // after sidebar widget 
-  }
-  return $params;
-}
-add_filter( 'dynamic_sidebar_params', 'wordpress_widgets_booststrapped_widget_params', 99 );
-// */
-
-
-/******************************************************************************************/
-/*   Sidebar Defaults - Filter Genesis Sidebar Default Markup to add Panels               */
-/******************************************************************************************/
-//*
-function gb3_register_sidebar_defaults( $defaults ) {
-	$defaults['before_widget'] = '<section id="%1$s" class="panel panel-default widget %2$s"><div class="panel-body widget-wrap">';
-        $defaults['after_widget'] = '</div></section>';
-	return $defaults;
-}
-add_filter( 'genesis_register_widget_area_defaults', 'gb3_register_sidebar_defaults', 99);
-// */
-
-
-
 
 
 
