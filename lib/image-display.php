d3fca189ba68bc5f843084bdc0c40c445718d3d4
Bryan Willis <bryanwillis1@gmail.com>
2016-01-05 22:13:20 -0600

    / ‘lib/image-display.php’

--- lib/image-display.php
+++ lib/image-display.php
@@ -9,22 +9,3 @@ function bsg_archive_image_attr_filter( $attributes ) {
     $attributes['class'] .= ' wp-post-image archive-featured-image img-responsive';
     return $attributes;
 }
-
-
-
-// Display Single
-add_action( 'genesis_entry_header', 'bsg_single_featured_image', 5 );
-function bsg_single_featured_image() {
-    global $post;
-    if ( ! is_singular() ) {
-        return;
-    }
-    if ( ! has_post_thumbnail() ) {
-        return;
-    }
-    $featured_image_attr = apply_filters( 'bsg-featured-image-attr', array(
-        'class' => 'single-featured-image img-responsive'
-    ) );
-    $size = apply_filters( 'bsg-featured-image', 'bsg-featured-image' );
-    the_post_thumbnail( $size, $featured_image_attr );
-}
