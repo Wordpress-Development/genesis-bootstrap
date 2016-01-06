c8c0150f1a8707b0cecbc45e135c298e6d0003ec
Bryan Willis <willis.bryan@live.com>
2016-01-06 02:54:57 -0600

    Update grid.php

--- lib/functions/grid.php
+++ lib/functions/grid.php
@@ -72,17 +72,3 @@ function bsg_add_markup_class( $attr, $context ) {
     $attr['class'] .= ' ' . implode( ' ', $classes_array );
     return $attr;
 }
-
-
-
-
-
-// Should we run this only in the loop?
-add_action('genesis_entry_header', 'gb3_entry_archive_panel_wrapper_close', 1);
-function gb3_entry_archive_panel_wrapper_close() {
-    echo '<div class="panel-body">';
-}
-add_action('genesis_entry_footer', 'gb3_entry_archive_panel_wrapper_open' , 999);
-function gb3_entry_archive_panel_wrapper_open() {
-    echo '</div>';
-}
