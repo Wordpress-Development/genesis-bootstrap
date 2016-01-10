800ef864e69b3a532e147dbcac1519a9edbbbf3d
Bryan Willis <willis.bryan@live.com>
2016-01-10 00:35:00 -0600

    Update layouts.php

--- lib/functions/layouts.php
+++ lib/functions/layouts.php
@@ -36,7 +36,48 @@ function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
     }
     return $classes_to_add;
 };
+
+
+
+
 function bsg_modify_classes_based_on_template( $classes_to_add, $context, $attr ) {
     $classes_to_add = bsg_layout_options_modify_classes_to_add( $classes_to_add );
     return $classes_to_add;
 }
+
+
+
+
+/*
+function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
+	
+	$layout = genesis_site_layout();
+	switch( $layout ) {
+		
+		case 'full-width-content' :	
+			$classes_to_add['content'] = 'col-sm-12';
+    			break;
+		case 'sidebar-content' :   	
+        		$classes_to_add['content'] = 'col-sm-12 col-md-8 col-lg-9 col-md-push-4 col-lg-push-3';
+        		$classes_to_add['sidebar-primary'] = 'hidden-xs hidden-sm col-md-4 col-lg-3 col-md-pull-8 col-lg-pull-9';
+    			break;
+		case 'content-sidebar-sidebar' :
+			$classes_to_add['content'] = 'col-sm-6';
+        		$classes_to_add['sidebar-primary'] = 'col-sm-3';
+        		$classes_to_add['sidebar-secondary'] = 'col-sm-3';
+        		break;
+		case 'sidebar-sidebar-content' :
+			$classes_to_add['content'] = 'col-sm-6 col-sm-push-6';
+        		$classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-pull-3';
+        		$classes_to_add['sidebar-secondary'] = 'col-sm-3 col-sm-pull-9';
+			break;
+		case 'sidebar-content-sidebar' :   	
+        		$classes_to_add['content'] = 'col-sm-6 col-sm-push-3';
+        		$classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-push-3';
+        		$classes_to_add['sidebar-secondary'] = 'col-sm-3 col-sm-pull-9';
+	    		break;
+	    		
+	}
+      	return $widget_output;
+}
+// */
