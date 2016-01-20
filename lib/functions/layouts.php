c1b532205a0e095e078570d6f6db1b3613baee70
Bryan Willis <willis.bryan@live.com>
2016-01-20 13:00:47 -0600

    Update layouts.php

--- lib/functions/layouts.php
+++ lib/functions/layouts.php
@@ -4,13 +4,13 @@ remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' )
 add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
 
 // modify bootstrap classes based on genesis_site_layout
-add_filter('bsg-classes-to-add', 'bsg_modify_classes_based_on_template', 10, 3);
-function bsg_modify_classes_based_on_template( $classes_to_add, $context, $attr ) {
-    $classes_to_add = bsg_layout_options_modify_classes_to_add( $classes_to_add );
-    return $classes_to_add;
+add_filter('bw_clean_classes_output', 'bw_modify_classes_based_on_template', 11, 3);
+function bw_modify_classes_based_on_template( $classes, $context, $attr ) {
+    $classes = bw_layout_options_modify_classes_to_add( $classes );
+    return $classes;
 }
 
-function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
+function bw_layout_options_modify_classes_to_add( $classes_to_add ) {
     $layout = genesis_site_layout();
     // full-width-content   
     if ( 'full-width-content' === $layout ) {
