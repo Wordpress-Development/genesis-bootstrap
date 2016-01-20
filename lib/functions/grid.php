a455a9c588994af2c61a8ff4cf63b614aaf22aae
Bryan Willis <willis.bryan@live.com>
2016-01-20 12:54:45 -0600

    Update grid.php

--- lib/functions/grid.php
+++ lib/functions/grid.php
@@ -1,72 +1,73 @@
 <?php
 
-// add bootstrap classes
-$genesis_atts = array(
-                  'nav-primary',
-                  'nav-secondary',
-                  'site-header',
-                  'site-inner',
-                  'content-sidebar-wrap',
-                  'content',
-                  'sidebar-primary',
-                  'sidebar-secondary',
-                  'archive-pagination',
-                  'entry-content',
-                  'entry-pagination',
-                  'site-footer',
-                  'nav-footer',
-                  'comment-reply',
-                  'comment-header',
-                  'comment-time-link',
-                  'comment-media',
-                  'comment',
-                  'comment-list',
-                  'widget',
-                  'entry',
-		  'jumbotron-header'
-
-    		);
-foreach ( $genesis_atts as $context ) {
-	$context = "genesis_attr_$context";
-	add_filter( $context, 'bsg_add_markup_class', 10, 2 );
+/**
+ * Adds Filters Automatically from Array Keys
+ */
+add_action('genesis_meta', 'bw_add_array_filters_genesis_attr');
+function bw_add_array_filters_genesis_attr()
+{
+	$filters = bw_merge_genesis_attr_classes($classes);
+	foreach(array_keys($filters) as $context) {
+		$context = "genesis_attr_$context";
+		add_filter($context, 'bw_add_markup_sanitize_classes', 10, 2);
+	}
 }
 
-function bsg_add_markup_class( $attr, $context ) {
-    $classes_to_add = apply_filters ('bsg-classes-to-add',
-        array(
-            'nav-primary'               => 'navbar navbar-inverse navbar-static-top',
-            'nav-secondary'             => 'navbar navbar-default navbar-static-top',
-            'site-header'               => '',
-            'site-inner'                => '',
-            'site-footer'               => '',
+
+/**
+ * Clean classes output
+ */
+function bw_add_markup_sanitize_classes($attr, $context)
+{
+	$classes = array();
+	if (has_filter('bw_clean_classes_output')) {
+		$classes = apply_filters('bw_clean_classes_output', $classes, $context, $attr);
+	}
+	$value = isset($classes[$context]) ? $classes[$context] : array();
+	if (is_array($value)) {
+		$classes_array = $value;
+	}
+	else {
+		$classes_array = explode(' ', (string)$value);
+	}
+	$classes_array = array_map('sanitize_html_class', $classes_array);
+	$attr['class'].= ' ' . implode(' ', $classes_array);
+	return $attr;
+}
+
+
+/**
+ * Default array of classes to add
+ */
+function bw_merge_genesis_attr_classes($classes)
+{
+	$classes = array(
+            'nav-primary'               => 'navbar navbar-default navbar-static-top',
+            'nav-secondary'             => 'navbar navbar-inverse navbar-static-top',
+            'site-header'               => 'container',
+            'site-inner'                => 'container',
+            'site-footer'               => 'container',
             'content-sidebar-wrap'      => 'row',
-            'content'                   => 'col-sm-12 col-md-8 col-lg-9',
-            'sidebar-primary'           => 'hidden-sm col-md-4 col-lg-3',
+            'content'                   => 'hidden-xs col-sm-8 col-md-8 col-lg-9',
+            'sidebar-primary'           => 'col-xs-12 col-sm-3 col-md-4 col-lg-3',
             'archive-pagination'        => 'clearfix',
             'entry-content'             => 'clearfix',
-            'entry-pagination'          => 'clearfix bsg-pagination-numeric',
-            'comment'                   => 'comment-body media',
-            'comment-reply'             => 'reply text-muted small pull-right',
-            'comment-header'            => 'media-heading',
-            'comment-time-link'         => 'text-muted small',
-            'comment-media'             => 'media-left',
-            'nav-footer'                => 'pull-right',
-            'comment-list'              => 'list-unstyled',
-            'widget'                    => 'panel panel-default',
-            'entry'                     => 'panel panel-default',
-            'jumbotron-header'          => 'fullwidth'
+            'entry-pagination'          => 'clearfix bsg-pagination-numeric'
+        );
+	if (has_filter('bw_add_classes')) {
+		$add_classes = apply_filters('bw_add_classes', $add_classes);
+		$classes = wp_parse_args($classes, $add_classes);
+	}
+	return $classes;
+}
 
-        ),
-        $context,
-        $attr
-    );
-    $value = isset( $classes_to_add[ $context ] ) ? $classes_to_add[ $context ] : array();
-    if ( is_array( $value ) ) {
-        $classes_array = $value;
-    } else {
-        $classes_array = explode( ' ', (string) $value );
-    }
-    $classes_array = array_map( 'sanitize_html_class', $classes_array );
-    $attr['class'] .= ' ' . implode( ' ', $classes_array );
-    return $attr;
+
+/**
+ * Adds classes array to bsg_add_markup_class() for cleaning
+ */
+add_filter('bw_clean_classes_output', 'bw_modify_classes_based_on_extras', 10, 3);
+function bw_modify_classes_based_on_extras($classes, $context, $attr)
+{
+	$classes = bw_merge_genesis_attr_classes($classes);
+	return $classes;
 }
