db8e21630b029746dacdc95d202374766ba530dd
Bryan Willis <willis.bryan@live.com>
2016-01-27 16:18:54 -0600

    Update post.php

--- lib/templates/post.php
+++ lib/templates/post.php
@@ -1,22 +1,31 @@
 <?php
 
 // Should we run this only in the loop since it messes with genesis widgets
-add_action('genesis_meta', 'bw_genesis_add_bootstrap_entry_wrap');
-function bw_genesis_add_bootstrap_entry_wrap() {
-    add_action('genesis_entry_header', 'gb3_entry_archive_panel_wrapper_open', 4);
-    add_action('genesis_entry_footer', 'gb3_entry_archive_panel_wrapper_close' , 55);
-    add_filter('bw_add_classes', 'bw_custom_entry_wrap');
+add_action('genesis_meta', 'gb3_add_entry_wrap_markup');
+function gb3_add_entry_wrap_markup() {
+	add_action('genesis_entry_header', 'gb3_entry_wrap_open', -1);
+	add_action('genesis_entry_footer', 'gb3_entry_wrap_close' , 55);
+    	add_filter('bw_add_classes', 'gb3_entry_wrap_classes');
 }
-function gb3_entry_archive_panel_wrapper_open() {
-    printf( '<div %s>', genesis_attr( 'entry-body' ) );
+
+
+/* Content Body */
+function gb3_entry_wrap_open() {
+    printf( '<div %s>', genesis_attr( 'entry-outer-wrap' ) );
+    printf( '<div %s>', genesis_attr( 'entry-inner-wrap' ) );
 }
-function gb3_entry_archive_panel_wrapper_close() {
-    echo '</div>';
+
+function gb3_entry_wrap_close() {
+    echo '</div></div>';
 }
-function bw_custom_entry_wrap($classes) {
-    $classes = array( 
-        'entry'          => 'panel panel-default',
-	    'entry-body'     => 'panel-body'
+
+
+// COMMENT CLASS FILTERS - you can modify this in your theme
+add_filter('bw_add_classes', 'gb3_entry_wrap_classes');
+function gb3_entry_wrap_classes($classes) {
+    $new_classes = array( 
+            'entry-outer-wrap'        => 'thumbnail',
+            'entry-inner-wrap'        => 'caption'
     );
-    return $classes;
+    return wp_parse_args($new_classes, $classes);
 }
