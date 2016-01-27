3592247337093bb02bd8c4edb3c292970d3a5976
Bryan Willis <willis.bryan@live.com>
2016-01-27 15:28:54 -0600

    Update nav.php

--- lib/templates/nav.php
+++ lib/templates/nav.php
@@ -275,33 +275,25 @@ ul.dropdown-menu ul.dropdown-menu{top:0;left:100%;}
 
 
 
-add_action('wp_footer', 'multilevel_dropdown_menu', 9999);
+add_action('wp_footer', 'multilevel_dropdown_menu', 999);
 function multilevel_dropdown_menu(){
 ?>
 <script type="text/javascript">
-(function ( $ ) {
-	"use strict";
-$(function () {
-$('ul.dropdown-menu .dropdown>a').on('click', function(event) {
-    // Avoid following the href location when clicking
-    event.preventDefault(); 
-    // Avoid having the menu to close when clicking
-    event.stopPropagation(); 
-	
-	// find parent element (<li> tag)
-	var cur_nav_element=$(this).parent();
-	cur_nav_element.parent().find('li.dropdown').not($(this).parent()).removeClass('open');
-	cur_nav_element.toggleClass('open');
-});
-	
-$('ul.navbar-nav a.dropdown-toggle').on('click', function(event) {	
-	// find parent element (<li> tag)
-	var cur_nav_element=$(this).parent();
-	cur_nav_element.parent().find('li.dropdown').not($(this).parent()).removeClass('open');
-});	
-
-	});
-
+(function($) {
+    "use strict";
+    $(function() {
+        $('ul.dropdown-menu .dropdown>a').on('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            var cur_nav_element = $(this).parent();
+            cur_nav_element.parent().find('li.dropdown').not($(this).parent()).removeClass('open');
+            cur_nav_element.toggleClass('open');
+        });
+        $('ul.navbar-nav a.dropdown-toggle').on('click', function(event) {
+            var cur_nav_element = $(this).parent();
+            cur_nav_element.parent().find('li.dropdown').not($(this).parent()).removeClass('open');
+        });
+    });
 }(jQuery));
 </script>
 <?php
