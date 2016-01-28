c2aea52214adac47efd505d34dfda820232d6915
Bryan Willis <bryanwillis1@gmail.com>
2016-01-27 19:25:10 -0600

    / ‘lib/templates/nav.php’

--- lib/templates/nav.php
+++ lib/templates/nav.php
@@ -1,157 +1,53 @@
 <?php
 
 
+ /**
+  * Remove Header Defaults
+  */ 
+add_action('genesis_setup', 'bsg_remove_header_defaults', 15);
+function bsg_remove_header_defaults() {
+	unregister_sidebar( 'header-right' );
+	remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+	remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+	remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+	remove_action( 'genesis_header', 'genesis_do_header' );
+	remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+}
+
+
+
+/**
+  * Bootstrap Nav Classes
+  */
 add_filter('bw_add_classes', 'bsg_custom_nav_classes');
-function bsg_custom_nav_classes($classes) {
+function bsg_custom_nav_classes($classes) 
+{
     $new_classes = array( 
             'nav-primary'               => 'navbar navbar-default navbar-static-top',
             'nav-secondary'             => 'navbar navbar-inverse navbar-static-top',
-            'site-header'               => 'container',
+            'site-header'               => 'container'
     );
     return wp_parse_args($new_classes, $classes);
 }
 
 
+
 /**
- * Class Name: wp_bootstrap_navwalker
- * GitHub URI: https://github.com/twittem/wp-bootstrap-navwalker
- */
-class wp_bootstrap_navwalker extends Walker_Nav_Menu {
-	public function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$indent = str_repeat( "\t", $depth );
-		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
-	}
-	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-		if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="divider">';
-		} else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="divider">';
-		} else if ( strcasecmp( $item->attr_title, 'dropdown-header') == 0 && $depth === 1 ) {
-			$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
-		} else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
-			$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
-		} else {
-			$class_names = $value = '';
-			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-			$classes[] = 'menu-item-' . $item->ID;
-			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-			if ( $args->has_children )
-				$class_names .= ' dropdown';
-			if ( in_array( 'current-menu-item', $classes ) )
-				$class_names .= ' active';
-			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
-			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
-			$output .= $indent . '<li' . $id . $value . $class_names .'>';
-			$atts = array();
-			$atts['title']  = ! empty( $item->title )	? $item->title	: '';
-			$atts['target'] = ! empty( $item->target )	? $item->target	: '';
-			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
-			// If item has_children add atts to a.
-			if ( $args->has_children && $depth === 0 ) {
-				$atts['href']   		= '#';
-				$atts['data-toggle']	= 'dropdown';
-				$atts['class']			= 'dropdown-toggle';
-				$atts['aria-haspopup']	= 'true';
-			} else {
-				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
-			}
-
-			// do_action( 'wp_nav_menu_item_custom_fields', $item_id, $item, $depth, $args );
-
-			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
-			$attributes = '';
-			foreach ( $atts as $attr => $value ) {
-				if ( ! empty( $value ) ) {
-					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-					$attributes .= ' ' . $attr . '="' . $value . '"';
-				}
-			}
-			$item_output = $args->before;
-		
-            if ( ! empty( $item->attr_title ) ){
-				$item_output .= '<a'. $attributes .'><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
-			}else{
-				$item_output .= '<a'. $attributes .'>';
-			}
-			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-			// Add caret if element has children
-			$item_output .= ($args->has_children) ? ' <span class="caret"></span></a>' : '</a>';
-			$item_output .= '</a>';
-			$item_output .= $args->after;
-			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-			
-			
-		}
-	}
-	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
-        if ( ! $element )
-            return;
-        $id_field = $this->db_fields['id'];
-        // Display this element.
-        if ( is_object( $args[0] ) )
-           $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
-        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-    }
-	public static function fallback( $args ) {
-		if ( current_user_can( 'manage_options' ) ) {
-			extract( $args );
-			$fb_output = null;
-			if ( $container ) {
-				$fb_output = '<' . $container;
-				if ( $container_id )
-					$fb_output .= ' id="' . $container_id . '"';
-				if ( $container_class )
-					$fb_output .= ' class="' . $container_class . '"';
-				$fb_output .= '>';
-			}
-			$fb_output .= '<ul';
-			if ( $menu_id )
-				$fb_output .= ' id="' . $menu_id . '"';
-			if ( $menu_class )
-				$fb_output .= ' class="' . $menu_class . '"';
-			$fb_output .= '>';
-			$fb_output .= '<li><a href="' . admin_url( 'nav-menus.php' ) . '">Add a menu</a></li>';
-			$fb_output .= '</ul>';
-			if ( $container )
-				$fb_output .= '</' . $container . '>';
-			echo $fb_output;
-		}
-	}
-}
-
-
-
-
-
-unregister_sidebar( 'header-right' );
-remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
-remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
-remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
-remove_action( 'genesis_header', 'genesis_do_header' );
-remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
-
-
-
-
-
+  * Bootstrap Nav Markup
+  */ 
 add_filter( 'genesis_do_nav', 'genesis_child_nav', 10, 3 );
 add_filter( 'genesis_do_subnav', 'genesis_child_nav', 10, 3 );
-
-function genesis_child_nav($nav_output, $nav, $args){
-    
+function genesis_child_nav($nav_output, $nav, $args)
+{
     $args['depth'] = 3;
     $args['menu_class'] = 'nav navbar-nav';
     $args['fallback_cb'] = 'wp_bootstrap_navwalker::fallback';
     $args['walker'] = new wp_bootstrap_navwalker();
     
     $nav = wp_nav_menu( $args );
-
     $sanitized_location = sanitize_key( $args['theme_location'] );
-  	
-	
     $data_target = 'nav-collapse-' . $sanitized_location;
+
     $nav_markup = <<<EOT
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#{$data_target}">
@@ -161,15 +57,13 @@ function genesis_child_nav($nav_output, $nav, $args){
             <span class="icon-bar"></span>
           </button>
 EOT;
-
     if ( 'primary' === $sanitized_location ) {
         $nav_markup .= apply_filters( 'bsg_navbar_brand', bsg_navbar_brand_markup() );
     }
-        
-        $nav_markup .= '</div>';
-        $nav_markup .= '<div class="collapse navbar-collapse" id="'.$data_target.'">';
+    $nav_markup .= '</div>'; // .navbar-header
+    $nav_markup .= '<div class="collapse navbar-collapse" id="'.$data_target.'">';
 	$nav_markup .= $nav;
-        $nav_markup .= '</div>'; // .collapse .navbar-collapse
+    $nav_markup .= '</div>'; // .collapse .navbar-collapse
     
     $nav_markup_open  = sprintf( '<nav %s>', genesis_attr( 'nav-' . $sanitized_location ) );
     $nav_markup_open .= genesis_structural_wrap( 'menu-' . $sanitized_location, 'open', 0 );
@@ -183,12 +77,12 @@ EOT;
 
 
 
-
-
-
-  // add customizer controls
+/**
+ * Navbar Brand Image Customizer Controls
+ */ 
 add_action( 'customize_register', 'bsg_navbar_brand_logo_customize_register' );
-function bsg_navbar_brand_logo_customize_register( $wp_customize ) {
+function bsg_navbar_brand_logo_customize_register( $wp_customize ) 
+{
 	    $wp_customize->add_setting( 'brand_logo',
              array(
                 'default' => '',
@@ -211,17 +105,21 @@ function bsg_navbar_brand_logo_customize_register( $wp_customize ) {
 
 
 
+/**
+ * Navbar Brand Image Output
+ */ 
+function bsg_navbar_brand_markup() 
+{
+    $name = esc_attr( get_bloginfo( 'name' ) );
 
+    if ( get_theme_mod( 'brand_logo' ) ) {
+    	$brand = '<img src="'.get_theme_mod('brand_logo').'" alt="'.$name.'">';
+    } else {
+    	$brand = $name;
+    }
+	
+	return '<a class="navbar-brand" id="logo" title="'.esc_attr( get_bloginfo( 'description' ) ).'" href="'.esc_url( home_url( '/' ) ).'">'.$brand.'</a>';
 
-function bsg_navbar_brand_markup() {
-        $brand_logo = get_theme_mod( 'brand_logo' );
-        if ( $brand_logo ) {
-            $img = '<img src="'.$brand_logo.'" alt="'.esc_attr( get_bloginfo( 'name' ) ).'">';
-            $output = '<a class="navbar-brand" id="logo" title="'.esc_attr( get_bloginfo( 'description' ) ).'" href="'.esc_url( home_url( '/' ) ).'">'.$img.'</a>';
-        } else {
-            $output = '<a class="navbar-brand" id="logo" title="'.esc_attr( get_bloginfo( 'description' ) ).'" href="'.esc_url( home_url( '/' ) ).'">'.esc_attr(get_bloginfo( 'name' )).'</a>';
-        }
-        return $output;
 }
 
 
@@ -253,7 +151,6 @@ ul.dropdown-menu .caret {
 }
 
 @media (max-width: 767px) {
-	
 ul.dropdown-menu .caret {	
 	display: inline-block;
 	width: 0;
@@ -266,8 +163,10 @@ ul.dropdown-menu .caret {
 	}
 	
 }
-
-ul.dropdown-menu ul.dropdown-menu{top:0;left:100%;}
+ul.dropdown-menu ul.dropdown-menu{
+	top:0;
+	left:100%;
+}
 </style>
 <?php
 }
