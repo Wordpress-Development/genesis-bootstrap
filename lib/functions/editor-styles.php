d1d92bc8a74c15b1efd658cc08f9f7fe818e2429
Bryan Willis <bryanwillis1@gmail.com>
2016-01-27 19:56:18 -0600

    < ‘lib/extras/title-toggle.php’ > ‘_development/title-toggle.php’ / ‘config.codekit’ < ‘lib/templates/author-box.php’ > ‘lib/extras/author-box.php’ < ‘lib/functions/jquery-cdn.php’ > ‘lib/extras/jquery-cdn.php’ < ‘lib/functions/scripts-to-footer.php’ > ‘lib/extras/scripts-to-footer.php’ / ‘lib/functions/editor-styles.php’ < ‘lib/widgets/responsive-video-embeds.php’ > ‘lib/templates/responsive-embed-widget.php’ < ‘lib/widgets/search-form.php’ > ‘lib/templates/search-form.php’ < ‘lib/widgets/sidebars.php’ > ‘lib/templates/sidebars.php’ < ‘lib/widgets/grid-columns.php’ > ‘lib/templates/widget-columns.php’ ...

--- lib/functions/editor-styles.php
+++ lib/functions/editor-styles.php
@@ -12,15 +12,10 @@
 /** Add Bootstrap styles */
 add_filter( 'mce_css', 'twbsg_mce_css' );
 function twbsg_mce_css( $mce_css ) {
-	if (defined('TWBSG_CSS')) {
-		$plugin_css = TWBSG_CSS;
-	} else {
-		$plugin_css = plugins_url('bootstrap/css/bootstrap.min.css', __DIR__);
-	}
 	if ( ! empty( $mce_css ) ) {
 		$mce_css .= ',';
 	}
-	$mce_css .= $plugin_css;
+	$mce_css .= $plugins_url('bootstrap/css/bootstrap.min.css', __DIR__);;
 	return $mce_css;
 }
 
