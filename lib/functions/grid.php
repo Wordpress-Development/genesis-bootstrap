4328c65b5f96d4e4f6bb6e1761e7236e0f6b9b77
Bryan Willis <willis.bryan@live.com>
2016-01-31 01:00:16 -0600

    Update grid.php

--- lib/functions/grid.php
+++ lib/functions/grid.php
@@ -65,3 +65,90 @@ function bw_modify_classes_based_on_extras($classes, $context, $attr)
     $classes = bw_merge_genesis_attr_classes($classes);
     return $classes;
 }
+
+
+
+
+
+
+/**
+ * Add theme support for structural wraps
+ */
+add_theme_support( 'genesis-structural-wraps', 
+		array( 
+			'menu-primary', 
+			'menu-secondary',
+			'footer',
+			'jumbotron-inner',
+			'site-inner',
+    	'header'
+		) 
+);
+
+
+/**
+ * Filter genesis_attr_structural-wrap to use BS .container-fluid classes
+ */
+add_filter( 'genesis_attr_structural-wrap', 'bsg_attributes_structural_wrap' );
+function bsg_attributes_structural_wrap( $attributes ) {
+    $attributes['class'] = 'container';
+    return $attributes;
+}
+
+
+/**
+ * Add structural-wrap replacement utility function to use 
+ * Bootstrap responsive .container class
+ *
+ * Useage: add_filter( 'genesis_structural_wrap-{$context}', 'bsg_wrap_container_fluid');
+ */
+function bsg_wrap_container_fluid( $output, $original_output ) {
+	if ( $original_output == 'open' ) {
+	   	$output = sprintf( '<div %s>', genesis_attr( 'container-fluid' ) );
+	}
+	return $output;
+}
+
+
+/**
+ * Helper function to apply to all areas
+ *
+ * Useage: add_filter( 'genesis_structural_wrap-{$context}', 'bsg_wrap_container_fluid');
+ */
+function bsg_wrap_all_container_fluid() {
+ $genesis_atts = array(
+     'menu-primary',
+     'menu-secondary',
+     'footer',
+     'jumbotron-inner',
+     'site-inner'
+     );
+ foreach ( $genesis_atts as $context ) {
+     $context = "genesis_structural_wrap-$context";
+     add_filter( $context, 'bsg_wrap_container_fluid', 16, 2 );
+ }
+}
+
+
+
+
+add_action( 'genesis_meta', 'bsgen_structural_wrap_fluid_site_inner' );
+function bsgen_structural_wrap_fluid_site_inner(){
+  add_filter( 'genesis_structural_wrap-site-inner', 'bsg_wrap_container_fluid', 99, 2);
+}
+
+
+
+
+/**
+ * Example of using the Above Helper
+ */
+ 
+ /*
+function do_genesis_wrap() {
+   if (is_home() ) {
+       bsg_wrap_all_container_fluid();
+   }
+}
+add_action('genesis_before', 'do_genesis_wrap');
+*/
