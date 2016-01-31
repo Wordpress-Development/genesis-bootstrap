94cd1803870fb5ccb61321a47f19e74e1b157174
Bryan Willis <willis.bryan@live.com>
2016-01-31 00:40:50 -0600

    Update bootstrap-genesis-css.php

--- lib/functions/bootstrap-genesis-css.php
+++ lib/functions/bootstrap-genesis-css.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * Enqueued CSS Stylesheets
+ */
+remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+add_action( 'wp_enqueue_scripts', 'twbsg_load_stylesheet_bootstrap' ); 
+add_action( 'wp_enqueue_scripts', 'twbsg_load_stylesheet_theme_tweaks', 99 );
 
-add_action( 'wp_enqueue_scripts', 'bsg_theme_default_style_fixes', 999 ); 
-function bsg_theme_default_style_fixes() {
+function twbsg_load_stylesheet_bootstrap() {
+    $stylesheet = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+    $stylesheet = apply_filters( 'bsg_css_url', $stylesheet );
+    wp_enqueue_style( 'bootstrap', $stylesheet, array(), false );
+}
+
+function twbsg_load_stylesheet_tweaks() {
     $stylesheet = plugins_url('/assets/css/bootstrap-genesis.css', __DIR__); // plugin_dir_url( __FILE__ )
-    wp_enqueue_style( 'bsg-theme-css', $stylesheet, array(), false );
+    wp_enqueue_style( 'theme-tweaks', $stylesheet, array(), false );
 }
