151684be66fdc5537a08105f8b57416d92b9d1f7
Bryan Willis <willis.bryan@live.com>
2016-03-10 15:31:39 -0600

    Update layouts.php

--- lib/functions/layouts.php
+++ lib/functions/layouts.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Willis\BootGen\Layouts;
+
+use Willis\BootGen\Utils;
 	
 remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
 add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
